b06d58b636ca94769a7b16d270842a977359bca3
oliver <kunhan945@gmail.com>
2022-06-17 11:17:09 +0800

    django graphql books

---
+++ books/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
